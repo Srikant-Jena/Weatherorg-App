49729aab9251f77c616d2b513c12b6fea68600bf
Srikant <srikantddazzler387@gmail.com>
2022-08-23 14:28:20 +0530

    Third Commit

--- weatherProject/settings.py
+++ weatherProject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["srikantweatherapp.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["srikantweatherapp.herokuapp.com","loclhost"]
 
 
 # Application definition
