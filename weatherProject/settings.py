cca4d37cc7b9ca0517ec2fcc007e9a8d20786007
Srikant <srikantddazzler387@gmail.com>
2023-01-10 21:06:28 +0530

    Final commit 2

--- weatherProject/settings.py
+++ weatherProject/settings.py
@@ -122,8 +122,8 @@ MEDIA_URL = '/images/'
 
 
 #deploy to  vercel /static working directory
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
